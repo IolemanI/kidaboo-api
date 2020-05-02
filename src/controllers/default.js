c28b740397c0bf589afd31c9bf7d8bbd65113ac7
Oleksii Manachynskyi <aom-95@live.com>
2020-05-02 23:26:30 +0300

    add logs for f** heroku

--- src/controllers/default.js
+++ src/controllers/default.js
@@ -3,6 +3,8 @@ import {controller, get} from 'koa-dec-router';
 export default @controller('/') class ProjectsCtrl {
   @get('/')
   async healthCheck(ctx) {
+    console.log('healthCheck');
+
     ctx.ok('OK');
   }
 }
